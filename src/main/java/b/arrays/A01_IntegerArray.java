bc43777d55e3d75027bfc09969e2930105d76e7a
bulentgeckin <bulentgeckin@gmail.com>
2020-04-09 16:57:35 -0400

    .

---
+++ src/main/java/b/arrays/A01_IntegerArray.java
@@ -0,0 +1,25 @@
+package b.arrays;
+
+import java.util.Arrays;
+
+public class A01_IntegerArray {
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 2, 3, 4 };
+		int b[] = { 4, 5, 6, 7 };
+		
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		
+		a[3]=9;
+		System.out.println(a[3]);
+		
+
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(b));
+
+	}
+}
